a3106b5c8fecc09a360b977a961ce1087633ad03
Carlos <cg.perezaranda@uma.es>
2024-05-30 19:33:26 +0200

    Cambiada la pagina de inicio para incluir un background

--- link_bio/link_bio/link_bio.py
+++ link_bio/link_bio/link_bio.py
@@ -6,8 +6,9 @@ from link_bio.views.links.links_teclados import links_teclados
 from link_bio.components.separator import separator
 from link_bio.components.footer import footer
 import link_bio.styles.styles as styles
-from link_bio.styles.styles import Size as Size
+from link_bio.styles.styles import Size as size
 from link_bio.components.giliginglesgalery import giliginglesgalery
+from link_bio.styles.colors import Color as color
 
 
 class State(rx.State):
@@ -16,8 +17,9 @@ class State(rx.State):
 def index() -> rx.Component:
 
     return rx.center(
-        rx.vstack(
-            navbar(),        
+        rx.flex(
+        navbar(),       
+        rx.flex(                             
             rx.vstack(
                 header(),
                 separator("Mis maquetas"),
@@ -25,18 +27,37 @@ def index() -> rx.Component:
                 separator("Mis Teclados"),
                 links_teclados(),
                 max_width = styles.MAX_WIDTH,
-                margin_y = styles.Size.LARGE.value
-                ),        
-                footer(),
-            align = "center",
-            text_align = "center",
-            width = "100%",
-        )
+                margin_y = styles.Size.LARGE.value,
+            ),        
+            width = "30%",
+            background_color = color.BACKGROUND.value,
+            border_radius = size.DEFAULT.value, 
+            margin = size.LARGE.value,
+            padding_x = size.LARGE.value,
+            padding_y = size.VVSMALL.value,
+            direction="column",
+            align = "start",
+            ),
+        footer(),
+        background = "center/cover url('/background.jpg')",
+        width="100%",
+        height="100%",
+        position="fixed",
+        z_index = "998",
+        top = "0",
+        direction = "column",
+        align = "center",
+        ),
     )
+    
+    
 
 app = rx.App(
     stylesheets=styles.STYLESHEETS,
     style = styles.BASE_STYLE,
+    theme=rx.theme(
+        appearance="light", has_background=True, radius="large", accent_color="teal"
+    )
 )
 app.add_page(index)
 app.add_page(giliginglesgalery, route = "/giliginglesgalery")
