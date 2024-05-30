510480c924d1e639822ab6dba3739e4b71420f66
CGPerezAranda <cg.perezaranda@uma.es>
2024-05-30 17:12:58 +0200

    puestos los botones de adelante y atrás en la parte superior de la galería

--- link_bio/link_bio/components/countstate.py
+++ link_bio/link_bio/components/countstate.py
@@ -18,40 +18,44 @@ class CountState (rx.State):
 
 def giligingle(): 
     return rx.center(
-        rx.hstack( 
-            rx.button(
-                rx.icon("arrow_left"),
-                border_radius="1em",
-                box_shadow="#245445 0 15px 30px -10px",
-                background_image="linear-gradient(144deg,#245445,#75ffd3)",
-                box_sizing="border-box",
-                color="white",
-                opacity=1,
-                _hover={
-                    "opacity": 0.5,
-                },
+        rx.vstack(
+            rx.hstack( 
+                rx.button(
+                    rx.icon("arrow_left"),
+                    border_radius="1em",
+                    box_shadow="#245445 0 15px 30px -10px",
+                    background_image="linear-gradient(144deg,#245445,#75ffd3)",
+                    box_sizing="border-box",
+                    color="white",
+                    opacity=1,
+                    _hover={
+                        "opacity": 0.5,
+                    },
+                    on_click=CountState.decrement,
+                    width="3em",
+                    position="sticky",
+                ),
+                rx.button(
+                    rx.icon("arrow_right"),
+                    border_radius="1em",
+                    box_shadow="#245445 0 15px 30px -10px",
+                    background_image="linear-gradient(144deg,#245445,#75ffd3)",
+                    box_sizing="border-box",
+                    color="white",
+                    opacity=1,
+                    _hover={
+                        "opacity": 0.5,
+                    },
                 on_click=CountState.decrement,
                 width="3em",
+                position="sticky",
+                ),
             ),
             rx.image(
-                src=f"/images/Giligingles/Giligingle ({CountState.count}).jpg",
-                width=Size.PHOTO.value,
-                height="auto",
-                align="center",
-            ),
-            rx.button(
-                rx.icon("arrow_right"),
-                border_radius="1em",
-                box_shadow="#245445 0 15px 30px -10px",
-                background_image="linear-gradient(144deg,#245445,#75ffd3)",
-                box_sizing="border-box",
-                color="white",
-                opacity=1,
-                _hover={
-                    "opacity": 0.5,
-                },
-                on_click=CountState.decrement,
-                width="3em",
+                    src=f"/images/Giligingles/Giligingle ({CountState.count}).jpg",
+                    width=Size.PHOTO.value,
+                    height="auto",
+                    align="center",
             ),
             align="center",
         )
