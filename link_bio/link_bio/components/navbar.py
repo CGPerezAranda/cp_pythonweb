d0fe9b29dd58dcd995196db45ae7e31c1ebd8d30
CGPerezAranda <cg.perezaranda@uma.es>
2024-05-30 16:51:47 +0200

    Enlace del navbar erróneo

--- link_bio/link_bio/components/navbar.py
+++ link_bio/link_bio/components/navbar.py
@@ -40,7 +40,7 @@ def navbar() -> rx.Component:
                 font_family = "Manrope",
                 font_weight = "500",
                 ),
-                href="/giliginglesgallery.py",
+                href="/giliginglesgalery",
             ),
         rx.spacer(),
         rx.link(
