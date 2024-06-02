07870dac45c086d16b2bdbd9bfb5bccbb702ffda
CGPerezAranda <cg.perezaranda@uma.es>
2024-06-02 11:07:09 +0200

    links al avenger

---
+++ link_bio/link_bio/views/links/photo_links.py
@@ -0,0 +1,19 @@
+
+
+tbm3 = ["https://i.imgur.com/Zueckgk.jpeg","https://i.imgur.com/GXGqGCV.jpeg","https://i.imgur.com/GXGqGCV.jpeg",
+        "https://i.imgur.com/9KWGtVw.jpeg","https://i.imgur.com/9KWGtVw.jpeg","https://i.imgur.com/byCJrF5.jpeg",
+        "https://i.imgur.com/8DaQK4v.jpeg","https://i.imgur.com/RcvJZLN.jpeg","https://i.imgur.com/buNYhgh.jpeg",
+        "https://i.imgur.com/GrwEFgq.jpeg","https://i.imgur.com/S350cNQ.jpeg","https://i.imgur.com/Qjey7G1.jpeg",
+        "https://i.imgur.com/wCwPRmz.jpeg","https://i.imgur.com/ZAXaQ2U.jpeg","https://i.imgur.com/NnvIxkZ.jpeg",
+        "https://i.imgur.com/CffgnbP.jpeg","https://i.imgur.com/bzUXHNC.jpeg","https://i.imgur.com/smC4URy.jpeg",
+        "https://i.imgur.com/z1Rbdzz.jpeg","https://i.imgur.com/8WNZ0dh.jpeg","https://i.imgur.com/8WNZ0dh.jpeg",
+        "https://i.imgur.com/IMpVAkQ.jpeg","https://i.imgur.com/IMpVAkQ.jpeg","https://i.imgur.com/9QD5Kq6.jpeg",
+        "https://i.imgur.com/MDCe6F3.jpeg","https://i.imgur.com/i6bOSvf.jpeg","https://i.imgur.com/kX0UaGc.jpeg",
+        "https://i.imgur.com/wL7zoUn.jpeg","https://i.imgur.com/MWP4SZ7.jpeg","https://i.imgur.com/JLZTT2r.jpeg",
+        "https://i.imgur.com/R1wrgRP.jpeg","https://i.imgur.com/lLxP257.jpeg","https://i.imgur.com/iHNb3qw.jpeg",
+        "https://i.imgur.com/zmLz8wr.jpeg","https://i.imgur.com/NSNhUH1.jpeg","https://i.imgur.com/jWWn6kY.jpeg",
+        "https://i.imgur.com/DKq3BCl.jpeg","https://i.imgur.com/SnS1hr1.jpeg","https://i.imgur.com/WeQ05eO.jpeg",
+        "https://i.imgur.com/jPA41Is.jpeg","https://i.imgur.com/H1LAiUY.jpeg","https://i.imgur.com/aLW8Etn.jpeg",
+        "https://i.imgur.com/vGbOrkE.jpeg","https://i.imgur.com/vGbOrkE.jpeg","https://i.imgur.com/vGbOrkE.jpeg",
+        "https://i.imgur.com/RllEzIm.jpeg","https://i.imgur.com/hFeTb8Z.jpeg","https://i.imgur.com/esCeiuF.jpeg",
+        "https://i.imgur.com/avnVSxt.jpeg","https://i.imgur.com/oV0myCz.jpeg"]
